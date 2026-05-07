9089fddc569cd0635db5335cdd8ff7c19265c2d4
niranjan185 <niranjanreddyvennapusa001@gmail.com>
2026-05-07 17:59:15 +0530

    fixed vite railway host

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
-      '/api': {
-        target: 'https://task-management-system-production-6619.up.railway.app',
-        changeOrigin: true
-      }
-    }
-  }
-})
+      "/api": {
+        target: "https://task-management-system-production-6619.up.railway.app",
+        changeOrigin: true,
+      },
+    },
+  },
+
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 8080,
+    allowedHosts: ["refreshing-perfection-production-ebc7.up.railway.app"],
+  },
+});
